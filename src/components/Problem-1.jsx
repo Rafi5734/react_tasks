e490d4d6122d891bedac01fc8726397b9a9c6a78
Rafi5734 <kamrulhasun5734@gmail.com>
2023-05-22 11:12:40 +0600

    problem 1 done, but problem 2 20% done

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,195 +1,194 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from "react";
 
 const Problem1 = () => {
+  const [show, setShow] = useState("all");
 
-    const [show, setShow] = useState('all');
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [active, setActive] = useState([]);
+  const [completed, setCompleted] = useState([]);
 
-    const [name, setName] = useState('')
-    const [status, setStatus] = useState('')
-    const [tasks, setTasks] = useState([]);
-    const [active, setActive] = useState([])
-    const [completed, setCompleted] = useState([])
+  const handleClick = (val) => {
+    setShow(val);
 
-
-    const handleClick = (val) =>{
-        setShow(val);
-
-        if (val === 'active') {
-            const activeAll = tasks.filter((task) => task.status === "active");
-            setActive(activeAll);
-        }
-
-        if (val === 'completed') {
-            const completedAll = tasks.filter(
-              (task) => task.status === "completed"
-            );
-            setCompleted(completedAll);
-        }
-
-
-         switch (val) {
-           case "all":
-             return tasks.sort((a, b) => {
-               if (a.status === "active" && b.status !== "active") {
-                 return -1;
-               } else if (a.status !== "active" && b.status === "active") {
-                 return 1;
-               } else if (
-                 a.status === "completed" &&
-                 b.status !== "completed"
-               ) {
-                 return -1;
-               } else if (
-                 a.status !== "completed" &&
-                 b.status === "completed"
-               ) {
-                 return 1;
-               } else {
-                 return 0;
-               }
-             });
-           default:
-             return tasks;
-         }
+    if (val === "active") {
+      const activeAll = tasks.filter((task) => task.status === "active");
+      setActive(activeAll);
     }
 
+    if (val === "completed") {
+      const completedAll = tasks.filter((task) => task.status === "completed");
+      setCompleted(completedAll);
+    }
 
-    console.log(tasks);
+    switch (val) {
+      case "all":
+        return tasks.sort((a, b) => {
+          if (a.status === "active" && b.status !== "active") {
+            return -1;
+          } else if (a.status !== "active" && b.status === "active") {
+            return 1;
+          } else if (a.status === "completed" && b.status !== "completed") {
+            return -1;
+          } else if (a.status !== "completed" && b.status === "completed") {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      default:
+        return tasks;
+    }
+  };
 
+  // console.log(tasks);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (name && status) {
+      const newTask = {
+        name: name,
+        status: status,
+      };
 
-        if (name && status) {
-             const newTask = {
-               name: name,
-               status: status,
-             };
-            
-             setTasks([...tasks, newTask]);
+      setTasks([...tasks, newTask]);
 
-             // Clear the input fields
-             setName("");
-             setStatus("");
-        }
-       
+      setName(" ");
+      setStatus(" ");
+    }
+  };
+
+  useEffect(() => {
+    const keyDownHandler = (event) => {
+      if (event.key === "Enter") {
+        // 👇️ call submit function here
+        handleSubmit();
+      }
     };
 
-    console.log(tasks);
+    document.addEventListener("keydown", keyDownHandler);
 
-    return (
-      <div className="container">
-        <div className="row justify-content-center mt-5">
-          <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
-          <div className="col-6 ">
-            <form
-              className="row gy-2 gx-3 align-items-center mb-4"
-              onSubmit={handleSubmit}
-            >
-              <div className="col-auto">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Name"
-                  name="name"
-                  onChange={(e) => {
-                    setName(e.target.value);
-                  }}
-                />
-              </div>
-              <div className="col-auto">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Status"
-                  name="status"
-                  onChange={(e) => {
-                    setStatus(e.target.value);
-                  }}
-                />
-              </div>
-              <div className="col-auto">
-                <button type="submit" className="btn btn-primary">
-                  Submit
-                </button>
-              </div>
-            </form>
-          </div>
-          <div className="col-8">
-            <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-              <li className="nav-item">
-                <button
-                  className={`nav-link ${show === "all" && "active"}`}
-                  type="button"
-                  onClick={() => handleClick("all")}
-                >
-                  All
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  className={`nav-link ${show === "active" && "active"}`}
-                  type="button"
-                  onClick={() => handleClick("active")}
-                >
-                  Active
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  className={`nav-link ${show === "completed" && "active"}`}
-                  type="button"
-                  onClick={() => handleClick("completed")}
-                >
-                  Completed
-                </button>
-              </li>
-            </ul>
-            <div className="tab-content"></div>
-            <table className="table table-striped ">
-              <thead>
-                <tr>
-                  <th scope="col">Name</th>
-                  <th scope="col">Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {show === "all" && (
-                  <>
-                    {tasks.map((task) => (
-                      <tr>
-                        <td>{task.name}</td>
-                        <td>{task.status}</td>
-                      </tr>
-                    ))}
-                  </>
-                )}
-                {show === "active" && (
-                  <>
-                    {active.map((task) => (
-                      <tr>
-                        <td>{task.name}</td>
-                        <td>{task.status}</td>
-                      </tr>
-                    ))}
-                  </>
-                )}
-                {show === "completed" && (
-                  <>
-                    {completed.map((task) => (
-                      <tr>
-                        <td>{task.name}</td>
-                        <td>{task.status}</td>
-                      </tr>
-                    ))}
-                  </>
-                )}
-              </tbody>
-            </table>
-          </div>
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, []);
+
+  //   console.log(tasks);
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+        <div className="col-6 ">
+          <form
+            className="row gy-2 gx-3 align-items-center mb-4"
+            onSubmit={handleSubmit}
+          >
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="name"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Status"
+                name="status"
+                onChange={(e) => {
+                  setStatus(e.target.value);
+                }}
+              />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="col-8">
+          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "all" && "active"}`}
+                type="button"
+                onClick={() => handleClick("all")}
+              >
+                All
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "active" && "active"}`}
+                type="button"
+                onClick={() => handleClick("active")}
+              >
+                Active
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "completed" && "active"}`}
+                type="button"
+                onClick={() => handleClick("completed")}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
+          <div className="tab-content"></div>
+          <table className="table table-striped ">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {show === "all" && (
+                <>
+                  {tasks.map((task, index) => (
+                    <tr key={index}>
+                      <td>{task.name}</td>
+                      <td>{task.status}</td>
+                    </tr>
+                  ))}
+                </>
+              )}
+              {show === "active" && (
+                <>
+                  {active.map((task, index) => (
+                    <tr key={index}>
+                      <td>{task.name}</td>
+                      <td>{task.status}</td>
+                    </tr>
+                  ))}
+                </>
+              )}
+              {show === "completed" && (
+                <>
+                  {completed.map((task, index) => (
+                    <tr key={index}>
+                      <td>{task.name}</td>
+                      <td>{task.status}</td>
+                    </tr>
+                  ))}
+                </>
+              )}
+            </tbody>
+          </table>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Problem1;
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,248 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Problem2 = () => {
+  const [contactData, setContactData] = useState();
+  const [usContactData, setUsContactData] = useState();
+  const [evenCheck, setEvenCheck] = useState(false);
+  let count = 1;
+  let usCount = 1;
 
-    return (
+  useEffect(() => {
+    fetchContactData();
+  }, []);
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+  useEffect(() => {
+    const findCountryByName = () => {
+      const country = contactData?.filter((item) => {
+        return item?.country?.name === "United States";
+      });
+      setUsContactData(country);
+      //   console.log(country);
+    };
+    findCountryByName();
+  }, [contactData]);
+
+  const fetchContactData = async () => {
+    try {
+      const response = await axios.get(
+        "https://contact.mediusware.com/api/contacts/"
+      );
+      setContactData(response.data.results);
+      console.log(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleCheck = (e) => {
+    // setEvenCheck(e.target.checked);
+    // console.log(evenCheck);
+    // if (evenCheck === false) {
+    //   const evenElements = contactData?.filter((item) => item?.id % 2 === 0);
+    //   setContactData(evenElements);
+    // }
+  };
+
+  //
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <button
+            className="btn btn-lg btn-outline-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal1"
+            type="button"
+          >
+            All Contacts
+          </button>
+          {/* Modal  */}
+          <div
+            class="modal fade"
+            id="exampleModal1"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel1"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button
+                    className="btn btn-lg btn-outline-primary me-5"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal1"
+                    type="button"
+                    style={{ backgroundColor: "#46139f" }}
+                  >
+                    All Contacts
+                  </button>
+                  <button
+                    className="btn btn-lg btn-outline-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal2"
+                    type="button"
+                    style={{ backgroundColor: "#ff7f50" }}
+                  >
+                    US Contacts
+                  </button>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                    style={{ border: "1px solid #46139f" }}
+                  ></button>
                 </div>
-                
+                <div class="modal-body">
+                  <h6>All countries contact</h6>
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Country</th>
+                        <th scope="col">Contact number</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {contactData?.map((item, index) => (
+                        <tr key={index}>
+                          <th scope="row">{item?.id}</th>
+                          <td>{item.country.name}</td>
+                          <td>{item.phone}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+                <div class="modal-footer">
+                  <div class="input-group mb-3">
+                    <div class="input-group-text">
+                      <input
+                        class="form-check-input mt-0"
+                        type="checkbox"
+                        value=""
+                        aria-label="Checkbox for following text input"
+                        onChange={handleCheck}
+                      />
+                      <label class="ms-3 form-check-label" for="invalidCheck">
+                        Only even
+                      </label>
+                    </div>
+                  </div>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="button" class="btn btn-primary">
+                    Save changes
+                  </button>
+                </div>
+              </div>
             </div>
+          </div>
+          <button
+            className="btn btn-lg btn-outline-warning"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal2"
+            type="button"
+          >
+            US Contacts
+          </button>
+
+          <div
+            class="modal fade"
+            id="exampleModal2"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel2"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button
+                    className="btn btn-lg btn-outline-primary me-5"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal1"
+                    type="button"
+                    style={{ backgroundColor: "#46139f" }}
+                  >
+                    All Contacts
+                  </button>
+                  <button
+                    className="btn btn-lg btn-outline-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal2"
+                    type="button"
+                    style={{ backgroundColor: "#ff7f50" }}
+                  >
+                    US Contacts
+                  </button>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                    style={{ border: "1px solid #46139f" }}
+                  ></button>
+                </div>
+                <div class="modal-body">
+                  <h6>US contacts</h6>
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Country</th>
+                        <th scope="col">Contact number</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {usContactData?.map((item, index) => (
+                        <tr key={index}>
+                          <th scope="row">{usCount++}</th>
+                          <td>{item.country.name}</td>
+                          <td>{item.phone}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+                <div class="modal-footer">
+                  <div class="input-group mb-3">
+                    <div class="input-group-text">
+                      <input
+                        class="form-check-input mt-0"
+                        type="checkbox"
+                        value=""
+                        aria-label="Checkbox for following text input"
+                      />
+                      <label class="ms-3 form-check-label" for="invalidCheck">
+                        Only even
+                      </label>
+                    </div>
+                  </div>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="button" class="btn btn-primary">
+                    Save changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
