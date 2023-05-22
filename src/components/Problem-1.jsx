aa09917228e8b2c59f684dc1f4c3bea6f7575a5c
Rafi5734 <kamrulhasun5734@gmail.com>
2023-05-22 09:06:03 +0600

    prohlem 1 solve

---
+++ src/components/Problem-1.jsx
@@ -0,0 +1,195 @@
+import React, {useState} from 'react';
+
+const Problem1 = () => {
+
+    const [show, setShow] = useState('all');
+
+    const [name, setName] = useState('')
+    const [status, setStatus] = useState('')
+    const [tasks, setTasks] = useState([]);
+    const [active, setActive] = useState([])
+    const [completed, setCompleted] = useState([])
+
+
+    const handleClick = (val) =>{
+        setShow(val);
+
+        if (val === 'active') {
+            const activeAll = tasks.filter((task) => task.status === "active");
+            setActive(activeAll);
+        }
+
+        if (val === 'completed') {
+            const completedAll = tasks.filter(
+              (task) => task.status === "completed"
+            );
+            setCompleted(completedAll);
+        }
+
+
+         switch (val) {
+           case "all":
+             return tasks.sort((a, b) => {
+               if (a.status === "active" && b.status !== "active") {
+                 return -1;
+               } else if (a.status !== "active" && b.status === "active") {
+                 return 1;
+               } else if (
+                 a.status === "completed" &&
+                 b.status !== "completed"
+               ) {
+                 return -1;
+               } else if (
+                 a.status !== "completed" &&
+                 b.status === "completed"
+               ) {
+                 return 1;
+               } else {
+                 return 0;
+               }
+             });
+           default:
+             return tasks;
+         }
+    }
+
+
+    console.log(tasks);
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (name && status) {
+             const newTask = {
+               name: name,
+               status: status,
+             };
+            
+             setTasks([...tasks, newTask]);
+
+             // Clear the input fields
+             setName("");
+             setStatus("");
+        }
+       
+    };
+
+    console.log(tasks);
+
+    return (
+      <div className="container">
+        <div className="row justify-content-center mt-5">
+          <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+          <div className="col-6 ">
+            <form
+              className="row gy-2 gx-3 align-items-center mb-4"
+              onSubmit={handleSubmit}
+            >
+              <div className="col-auto">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  name="name"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="col-auto">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Status"
+                  name="status"
+                  onChange={(e) => {
+                    setStatus(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="col-auto">
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="col-8">
+            <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+              <li className="nav-item">
+                <button
+                  className={`nav-link ${show === "all" && "active"}`}
+                  type="button"
+                  onClick={() => handleClick("all")}
+                >
+                  All
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  className={`nav-link ${show === "active" && "active"}`}
+                  type="button"
+                  onClick={() => handleClick("active")}
+                >
+                  Active
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  className={`nav-link ${show === "completed" && "active"}`}
+                  type="button"
+                  onClick={() => handleClick("completed")}
+                >
+                  Completed
+                </button>
+              </li>
+            </ul>
+            <div className="tab-content"></div>
+            <table className="table table-striped ">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {show === "all" && (
+                  <>
+                    {tasks.map((task) => (
+                      <tr>
+                        <td>{task.name}</td>
+                        <td>{task.status}</td>
+                      </tr>
+                    ))}
+                  </>
+                )}
+                {show === "active" && (
+                  <>
+                    {active.map((task) => (
+                      <tr>
+                        <td>{task.name}</td>
+                        <td>{task.status}</td>
+                      </tr>
+                    ))}
+                  </>
+                )}
+                {show === "completed" && (
+                  <>
+                    {completed.map((task) => (
+                      <tr>
+                        <td>{task.name}</td>
+                        <td>{task.status}</td>
+                      </tr>
+                    ))}
+                  </>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Problem1;
